3ad4bb9b769a11abf11866ef2799cb24a30775d0
klementtan <klementtan@gmail.com>
2019-07-24 22:49:07 +0700

    Add filter request body

--- xfers-swagger-api/app/controllers/json_w_yaml_controller.rb
+++ xfers-swagger-api/app/controllers/json_w_yaml_controller.rb
@@ -22,7 +22,7 @@ class JsonWYamlController < JsonController
   end
 
   def generate_doc(name)
-    generate_curr_paths(@yml[name.to_sym]).process_for_tag(name).process_for_params(name).generate_file_curr_json(name)
+    generate_curr_paths(@yml[name.to_sym]).process_for_tag(name).process_for_params(name).process_for_requestBody(name).generate_file_curr_json(name)
   end
 
   def generate_all
@@ -69,8 +69,34 @@ class JsonWYamlController < JsonController
     curr_oas = deep_copy(@curr_oas)
     curr_oas["paths"] = paths_json
     JsonWYamlController.new(@yml, @master_oas_wo_paths_json, @populate_nested_hash, curr_oas)
+
   end
 
+  def process_for_requestBody (name)
+    paths_json = deep_copy(@curr_oas["paths"])
+    paths_json.each { |endpoint, path_obj|
+      path_obj.each { |http_method, opp_obj|
+        next if opp_obj["requestBody"].nil?
+        opp_obj["requestBody"]["content"].each { |media_type, schema|
+          bybug if schema["schema"]["properties"].nil?
+          schema["schema"]["properties"].each { |field, value|
+            next if value["description"].nil?
+            description_arr = value["description"].to_s.split(" ")
+            next if !description_arr[0].include?("Only")
+            if !description_arr[1].to_s.include?(name.to_s)
+              schema["schema"]["properties"].delete(field)
+              puts("deleted " + field.to_s + " for " + name.to_s )
+            end
+          }
+          opp_obj.delete("requestBody") if schema["schema"]["properties"].nil?
+        }
+      }
+    }
+    puts("Finished filtering requestBody for " + name.to_s)
+    curr_oas = deep_copy(@curr_oas)
+    curr_oas["paths"] = paths_json
+    JsonWYamlController.new(@yml, @master_oas_wo_paths_json, @populate_nested_hash, curr_oas)
+  end
 
   def get_names
     @yml.keys
