306ca60f8c1a3708fd5ce7889dade334977dac5f
Corey Feng <corey@xfers.com>
2020-05-11 13:23:11 +0800

    Rename to Payment

--- react-page/src/oas_spec/experiment/index.js
+++ react-page/src/oas_spec/experiment/index.js
@@ -2,15 +2,14 @@ const combined = Object.assign(
     require("./base.json"),
     {
         paths: Object.assign(
-            require("./paths/direct_payment/queries.json"),
-            require("./paths/payment_collector/queries.json"),
-            require("./paths/direct_payment/virtual_account_bank_transfers.json"),
-            require("./paths/direct_payment/unique_amount_bank_transfers.json"),
+            require("./paths/payment/root.json"),
+            require("./paths/payment/virtual_account_bank_transfers.json"),
+            require("./paths/payment/unique_amount_bank_transfers.json"),
+            require("./paths/payment_collector/root.json"),
             require("./paths/payment_collector/fixed_virtual_accounts.json"),
         ),
         base_class: {
-            payment: require("./base_class/payment.json"),
-            direct_payment: require("./base_class/direct_payment.json")
+            payment: require("./base_class/payment.json")
         },
         components: {
             definitions: require("../indo/components/definitions.json"), // api v4 schemas use this for now
