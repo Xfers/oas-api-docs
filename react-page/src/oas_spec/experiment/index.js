8d4e9114ae329ecb5212b17d69f4284246cc1405
Corey Feng <corey@xfers.com>
2020-05-18 11:10:30 +0800

    Payment collector renaming

--- react-page/src/oas_spec/experiment/index.js
+++ react-page/src/oas_spec/experiment/index.js
@@ -6,11 +6,11 @@ const combined = Object.assign(
             require("./paths/payments/cancel.json"),
             require("./paths/direct_payment/virtual_account_bank_transfers.json"),
             require("./paths/direct_payment/unique_amount_bank_transfers.json"),
-            require("./paths/static_payment_reference/fixed_virtual_accounts.json"),
+            require("./paths/payment_collector/fixed_virtual_accounts.json"),
         ),
         base_class: {
             payment: require("./base_class/payment.json"),
-            static_payment_reference: require("./base_class/static_payment_reference.json"),
+            payment_collector: require("./base_class/payment_collector.json"),
         },
         components: {
             definitions: require("../indo/components/definitions.json"), // api v4 schemas use this for now
