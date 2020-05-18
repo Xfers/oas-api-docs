b5e19952b594782e970339821f23d38f345fff21
Corey Feng <corey@xfers.com>
2020-05-18 12:04:34 +0800

    Updates

--- precache-manifest.7efc71abe9487fba7063dcc3fb0087b1.js
+++ precache-manifest.7efc71abe9487fba7063dcc3fb0087b1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "120cb6fda6fcb47447e4580d7a7b879b",
+    "revision": "cc6971557c578009a4c4f21acaca0a3d",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.5a4c1816.chunk.css"
   },
   {
-    "revision": "9f46f1886deb48b94a6e",
+    "revision": "94187d55211af97c10e9",
     "url": "/static/css/main.924d20ab.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d63314ed.chunk.js"
   },
   {
-    "revision": "9f46f1886deb48b94a6e",
-    "url": "/static/js/main.98f1c79e.chunk.js"
+    "revision": "94187d55211af97c10e9",
+    "url": "/static/js/main.663cbbd2.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ee6506e9eafc14633ca2f0567811891.js"
+  "/precache-manifest.7efc71abe9487fba7063dcc3fb0087b1.js"
 );
 
 self.addEventListener('message', (event) => {
