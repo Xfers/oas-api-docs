ad45a7a98257b767b0fa19936862a9e373746734
yohanes-ffg <yohanes.lim@fazzfinancial.com>
2020-06-18 17:38:53 +0800

    binance and neu PG API sync

--- react-page/src/oas_spec/experiment/index.js
+++ react-page/src/oas_spec/experiment/index.js
@@ -5,7 +5,7 @@ const combined = Object.assign(
             require("./paths/payments/queries.json"),
             require("./paths/payment_methods/virtual_account_bank_transfers.json"),
             require("./paths/payment_methods/unique_amount_bank_transfers.json"),
-            require("./paths/payment_methods/fixed_virtual_accounts.json"),
+            // require("./paths/payment_methods/fixed_virtual_accounts.json"),
         ),
         base_class: {
             payment: require("./base_class/payment.json"),
