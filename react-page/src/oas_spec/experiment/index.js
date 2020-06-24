286533731f86b14759ca876f2dbb99c2ce22cdcd
yohanes-ffg <yohanes.lim@fazzfinancial.com>
2020-06-24 09:05:25 +0800

    PG API experiment refactor

--- react-page/src/oas_spec/experiment/index.js
+++ react-page/src/oas_spec/experiment/index.js
@@ -3,7 +3,7 @@ const combined = Object.assign(
     {
         paths: Object.assign(
             require("./paths/payments/queries.json"),
-            require("./paths/payment_methods/virtual_account_bank_transfers.json"),
+            // require("./paths/payment_methods/virtual_account_bank_transfers.json"),
             require("./paths/payment_methods/unique_amount_bank_transfers.json"),
             // require("./paths/payment_methods/fixed_virtual_accounts.json"),
         ),
